f6223e54f604458fef8ebeeacd25a8dfbfc86a10
kimyosunnyc <kimyosunnyc@gmail.com>
2016-07-05 10:18:43 +0900

    테스트 문구 삭제

--- wordpress/wp-content/themes/twentysixteen/header.php
+++ wordpress/wp-content/themes/twentysixteen/header.php
@@ -23,8 +23,6 @@
 
 <body <?php body_class(); ?>>
 
-<h1>테스트입니다.</h1>
-
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
